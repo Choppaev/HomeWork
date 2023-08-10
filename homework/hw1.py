4ef55e4c01a8b1f314a108db107505a3211ad3b0
Choppaev <141328027+Choppaev@users.noreply.github.com>
2023-08-10 22:10:38 +0600

    Update hw1.py

---
+++ homework/hw1.py
@@ -0,0 +1,18 @@
+def __init__(self, name, nickname, superpower, health_points, catchphrase):
+    self.name = name
+    self.nickname = nickname
+    self.superpower = superpower
+    self.health_points = health_points
+    self.catchphrase = catchphrase
+
+def print_name(self):
+    print(self.name)
+
+def double_health_points(self):
+    self.health_points *= 2
+
+def __repr__(self):
+    return f"Nickname: {self.nickname}, Superpower: {self.superpower}, Health Points: {self.health_points}"
+
+def __len__(self):
+    return len(self.catchphrase)
